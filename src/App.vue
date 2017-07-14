996795ddf7fe49e3d06549827c30334d90ff06bf
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-07-14 22:27:06 +0200

    Add CoreUI

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,85 @@
 <template>
-    <div id="app">
-        <h1>Hello ma tzr aze</h1>
+    <div class="app">
+        <div class="app-body">
+            <main class="main">
+                <div class="container-fluid">
+                    <div class="animated fadeIn">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-6">
+                                <div class="social-box facebook">
+                                    <i class="fa fa-facebook"></i>
+                                    <ul>
+                                        <li>
+                                            <strong>89k</strong>
+                                            <span>friends</span>
+                                        </li>
+                                        <li>
+                                            <strong>459</strong>
+                                            <span>feeds</span>
+                                        </li>
+                                    </ul>
+                                </div><!--/social-box-->
+                            </div><!--/.col-->
+                            <div class="col-md-3 col-sm-6">
+                                <div class="social-box twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <ul>
+                                        <li>
+                                            <strong>973k</strong>
+                                            <span>followers</span>
+                                        </li>
+                                        <li>
+                                            <strong>1.792</strong>
+                                            <span>tweets</span>
+                                        </li>
+                                    </ul>
+                                </div><!--/social-box-->
+                            </div><!--/.col-->
+                            <div class="col-md-3 col-sm-6">
+                                <div class="social-box linkedin">
+                                    <i class="fa fa-linkedin"></i>
+                                    <ul>
+                                        <li>
+                                            <strong>500+</strong>
+                                            <span>contacts</span>
+                                        </li>
+                                        <li>
+                                            <strong>292</strong>
+                                            <span>feeds</span>
+                                        </li>
+                                    </ul>
+                                </div><!--/social-box-->
+                            </div><!--/.col-->
+                            <div class="col-md-3 col-sm-6">
+                                <div class="social-box google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <ul>
+                                        <li>
+                                            <strong>894</strong>
+                                            <span>followers</span>
+                                        </li>
+                                        <li>
+                                            <strong>92</strong>
+                                            <span>circles</span>
+                                        </li>
+                                    </ul>
+                                </div><!--/social-box-->
+                            </div><!--/.col-->
+                        </div><!--/.row-->
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'widgets',
+        data () {
+            return {
+                msg: 'Widgets'
+            }
+        }
+    }
+</script>
