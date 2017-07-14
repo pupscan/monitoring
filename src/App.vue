93cec1124fb60dcd2cd0639988c9512ec6152174
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-07-14 22:57:28 +0200

    Add favicon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-    <div class="app">
-        <div class="app-body">
-            <main class="main">
-                <div class="container-fluid">
-                    <div class="animated fadeIn">
+
                         <div class="row">
-                            <div class="col-md-3 col-sm-6">
+                            <div class="col-sm-6 col-lg-3">
                                 <div class="social-box facebook">
                                     <i class="fa fa-facebook"></i>
+                                    <div class="chart-wrapper">
+                                        <social-box-chart-example :data="[65, 59, 84, 84, 51, 55, 40]" height="90"/>
+                                    </div>
                                     <ul>
                                         <li>
                                             <strong>89k</strong>
@@ -18,11 +17,15 @@
                                             <span>feeds</span>
                                         </li>
                                     </ul>
-                                </div><!--/social-box-->
+                                </div><!--/.social-box-->
                             </div><!--/.col-->
-                            <div class="col-md-3 col-sm-6">
+
+                            <div class="col-sm-6 col-lg-3">
                                 <div class="social-box twitter">
                                     <i class="fa fa-twitter"></i>
+                                    <div class="chart-wrapper">
+                                        <social-box-chart-example :data="[1, 13, 9, 17, 34, 41, 38]" height="90"/>
+                                    </div>
                                     <ul>
                                         <li>
                                             <strong>973k</strong>
@@ -33,11 +36,16 @@
                                             <span>tweets</span>
                                         </li>
                                     </ul>
-                                </div><!--/social-box-->
+                                </div><!--/.social-box-->
                             </div><!--/.col-->
-                            <div class="col-md-3 col-sm-6">
+
+                            <div class="col-sm-6 col-lg-3">
+
                                 <div class="social-box linkedin">
                                     <i class="fa fa-linkedin"></i>
+                                    <div class="chart-wrapper">
+                                        <social-box-chart-example :data="[78, 81, 80, 45, 34, 12, 40]" height="90"/>
+                                    </div>
                                     <ul>
                                         <li>
                                             <strong>500+</strong>
@@ -48,11 +56,15 @@
                                             <span>feeds</span>
                                         </li>
                                     </ul>
-                                </div><!--/social-box-->
+                                </div><!--/.social-box-->
                             </div><!--/.col-->
-                            <div class="col-md-3 col-sm-6">
+
+                            <div class="col-sm-6 col-lg-3">
                                 <div class="social-box google-plus">
                                     <i class="fa fa-google-plus"></i>
+                                    <div class="chart-wrapper">
+                                        <social-box-chart-example :data="[35, 23, 56, 22, 97, 23, 64]" height="90"/>
+                                    </div>
                                     <ul>
                                         <li>
                                             <strong>894</strong>
@@ -63,7 +75,7 @@
                                             <span>circles</span>
                                         </li>
                                     </ul>
-                                </div><!--/social-box-->
+                                </div><!--/.social-box-->
                             </div><!--/.col-->
                         </div><!--/.row-->
                     </div>
