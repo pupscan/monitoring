5db5c23c6176bc960401996a1ba2eb31a552988e
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-07-15 14:32:52 +0200

    Add new chart library: ECharts

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,102 @@
 <template>
-    <div class="row">
-        <div class="col-sm-6 col-lg-3">
-            <div class="social-box facebook">
-                <i class="fa fa-facebook"></i>
-                <div class="chart-wrapper">
-                    <social-chart :data="[65, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55, 40]"/>
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-sm-6 col-lg-3">
+                <div class="social-box facebook">
+                    <i class="fa fa-facebook"></i>
+                    <div class="chart-wrapper">
+                        <social-chart
+                                :data="[65, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55, 40]"/>
+                    </div>
+                    <ul>
+                        <li>
+                            <strong>89k</strong>
+                            <span>friends</span>
+                        </li>
+                        <li>
+                            <strong>459</strong>
+                            <span>feeds</span>
+                        </li>
+                    </ul>
                 </div>
-                <ul>
-                    <li>
-                        <strong>89k</strong>
-                        <span>friends</span>
-                    </li>
-                    <li>
-                        <strong>459</strong>
-                        <span>feeds</span>
-                    </li>
-                </ul>
+            </div>
+
+            <div class="col-sm-6 col-lg-3">
+                <div class="social-box twitter">
+                    <i class="fa fa-twitter"></i>
+                    <div class="chart-wrapper">
+                        <social-chart
+                                :data="[1, 13, 9, 17, 34, 41, 38, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55, 40]"/>
+                    </div>
+                    <ul>
+                        <li>
+                            <strong>973k</strong>
+                            <span>followers</span>
+                        </li>
+                        <li>
+                            <strong>1.792</strong>
+                            <span>tweets</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-sm-3 col-lg-2">
+                <div class="card card-inverse card-danger">
+                    <div class="card-block">
+                        <div class="h1 text-muted text-right mb-2"><i class="fa fa-usd fa-lg"></i></div>
+                        <div class="h4 mb-0">$ 420.000</div>
+                        <small class="text-muted text-uppercase font-weight-bold">Total funds</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3 col-lg-2">
+                <div class="card card-inverse card-success">
+                    <div class="card-block">
+                        <div class="h1 text-muted text-right mb-2"><i class="fa fa-users fa-lg"></i></div>
+                        <div class="h4 mb-0">3 498</div>
+                        <small class="text-muted text-uppercase font-weight-bold">Total backers</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3 col-lg-2">
+                <div class="card card-inverse card-warning">
+                    <div class="card-block">
+                        <div class="h1 text-muted text-right mb-2"><i class="fa fa-sticky-note fa-lg"></i></div>
+                        <div class="h4 mb-0">928</div>
+                        <small class="text-muted text-uppercase font-weight-bold">Total ZDesk tickets</small>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="col-sm-6 col-lg-3">
-            <div class="social-box twitter">
-                <i class="fa fa-twitter"></i>
-                <div class="chart-wrapper">
-                    <social-chart :data="[1, 13, 9, 17, 34, 41, 38, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55, 40]"/>
-                </div>
-                <ul>
-                    <li>
-                        <strong>973k</strong>
-                        <span>followers</span>
-                    </li>
-                    <li>
-                        <strong>1.792</strong>
-                        <span>tweets</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="col-sm-3 col-lg-2">
-            <div class="card card-inverse card-danger">
-                <div class="card-block">
-                    <div class="h1 text-muted text-right mb-2"><i class="fa fa-usd fa-lg"></i></div>
-                    <div class="h4 mb-0">$ 420.000</div>
-                    <small class="text-muted text-uppercase font-weight-bold">Total funds</small>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <h4 class="card-title mb-0">Indiegogo</h4>
+                                <div class="small text-muted">November 2016</div>
+                            </div>
+                        </div>
+                        <funding-chart class="chart-wrapper" style="height:300px;margin-top:40px;"></funding-chart>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="col-sm-3 col-lg-2">
-            <div class="card card-inverse card-success">
-                <div class="card-block">
-                    <div class="h1 text-muted text-right mb-2"><i class="fa fa-users fa-lg"></i></div>
-                    <div class="h4 mb-0">3 498</div>
-                    <small class="text-muted text-uppercase font-weight-bold">Total backers</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-3 col-lg-2">
-            <div class="card card-inverse card-warning">
-                <div class="card-block">
-                    <div class="h1 text-muted text-right mb-2"><i class="fa fa-sticky-note fa-lg"></i></div>
-                    <div class="h4 mb-0">928</div>
-                    <small class="text-muted text-uppercase font-weight-bold">Total ZenDesk tickets</small>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <h4 class="card-title mb-0">KissKissBankBank</h4>
+                                <div class="small text-muted">November 2016</div>
+                            </div>
+                        </div>
+                        <funding-chart class="chart-wrapper" style="height:300px;margin-top:40px;"></funding-chart>
+                    </div>
                 </div>
             </div>
         </div>
@@ -72,11 +105,14 @@
 
 <script>
     import SocialChart from './component/SocialBox'
+    import FundingChart from './component/FundingChart'
+
 
     export default {
         name: 'dashboard',
         components: {
-            SocialChart
+            SocialChart,
+            FundingChart
         }
     }
 </script>
---
+++ src/component/FundingChart.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="echarts">
+        <IEcharts :option="bar" @ready="onReady" @click="onClick"></IEcharts>
+    </div>
+</template>
+
+<script>
+    import IEcharts from 'vue-echarts-v3/src/full.vue'
+
+    // const brandPrimary = '#20a8d8'
+    const brandSuccess = '#4dbd74'
+    const brandInfo = '#63c2de'
+    const brandDanger = '#f86c6b'
+
+    function convertHex (hex, opacity) {
+        hex = hex.replace('#', '')
+        const r = parseInt(hex.substring(0, 2), 16)
+        const g = parseInt(hex.substring(2, 4), 16)
+        const b = parseInt(hex.substring(4, 6), 16)
+
+        const result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')'
+        return result
+    }
+
+    function random (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    export default {
+        props: [],
+        components: {
+            IEcharts
+        },
+        data: () => ({
+            bar: {
+                tooltip: {
+                    trigger: 'axis',
+                    position: function (pt) {
+                        return [pt[0], '10%'];
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '3%',
+                    bottom: '3%',
+                    top: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    axisLine: {
+                        lineStyle: {
+                            color: '#a1a5a6'
+                        }
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    axisLabel: {
+                        textStyle: {
+                            color: '#5c6061'
+                        }
+                    },
+                    data: ['M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S']
+                },
+                yAxis: {
+                    splitNumber: 3,
+                    axisLine: {
+                        lineStyle: {
+                            color: "#dde1e2"
+                        }
+                    },
+                    splitLine: {
+                        lineStyle: {
+                            color: "#dde1e2"
+                        }
+                    },
+                    axisTick: {
+                        length: 8,
+                        show: true,
+                        lineStyle: {
+                            color: "#dde1e2"
+                        }
+                    },
+                    axisLabel: {
+                        textStyle: {
+                            color: '#5c6061'
+                        }
+                    }
+                },
+                series: [
+                    {
+                        name: 'Funds',
+                        type: 'line',
+                        smooth: true,
+                        symbol: 'none',
+                        sampling: 'average',
+                        itemStyle: {
+                            normal: {
+                                color: brandInfo
+                            }
+                        },
+                        areaStyle: {
+                            normal: {
+                                color: convertHex(brandInfo, 10)
+                            }
+                        },
+                        data: [193, 138, 121, 54, 145, 106, 126, 59, 135, 166, 105, 125, 89, 128, 113, 102, 103, 196, 61, 130, 99, 56, 98, 137, 139, 83, 189, 106]
+                    },
+                    {
+                        name: 'Goals',
+                        type: 'line',
+                        smooth: true,
+                        symbol: 'none',
+                        sampling: 'average',
+                        itemStyle: {
+                            normal: {
+                                color: brandDanger,
+                                lineStyle: {
+                                    width: 2,
+                                    type: 'dashed'
+                                }
+                            },
+                            emphasis: {
+                                color: 'blue'
+                            }
+                        },
+                        data: [320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320, 320,]
+                    }
+                ]
+            }
+        }),
+        methods: {
+            onReady (instance) {
+                console.log(instance);
+            },
+            onClick (event, instance, echarts) {
+                console.log(arguments);
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .echarts {
+        height: 300px;
+    }
+</style>
